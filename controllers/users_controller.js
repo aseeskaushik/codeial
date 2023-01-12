ac02438d936468f434383f4e162d8bf30143c46a
Ashish Kaushik <ashishkaushik146@gmail.com>
2023-01-12 10:55:04 +0530

    rendered home page and profile page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 module.exports.profile= function(req,res){
-    res.end('<h1>User Profile</h1>');
+    
+    return res.render('profile',{
+        title:'Profile'
+    })
 }
 
 module.exports.posts= function(req,res){
