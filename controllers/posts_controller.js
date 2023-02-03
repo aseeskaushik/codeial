12f9a7f222743ee62ce16a2c90416c22589036e3
Ashish Kaushik <ashishkaushik146@gmail.com>
2023-02-03 10:56:56 +0530

    deleting  a post(authorized)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const Comment= require('../models/comment');
 
 // action to create posts in db from post form
 module.exports.create= function(req,res){
@@ -9,5 +10,21 @@ module.exports.create= function(req,res){
         if(err){console.log('Error in creating a post');return;}
 
         return res.redirect('back');
+    });
+}
+
+// action to delete posts from database
+module.exports.destroy= function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+          post.remove();
+
+          Comment.deleteMany({post: req.params.id},function(err){
+            return res.redirect('back');
+          });
+        }else{
+            return res.redirect('back');
+        }
     });
 }
