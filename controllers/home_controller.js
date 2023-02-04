034466ab5c45c9263e72d0a6aa8a52801474cc1d
Ashish Kaushik <ashishkaushik146@gmail.com>
2023-02-04 16:53:43 +0530

    converting controllers code to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,35 @@
 const Post= require('../models/post');
 const User= require('../models/user');
 
-module.exports.home=function(req,res){
-    // console.log(req.cookies);
-    // res.cookie('user_id','14');
-
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title:'Codeial|Home',
-    //         posts: posts
-    //     });
-    // });
-
+module.exports.home=async function(req,res){
+try{
     // Populate the user of each post
-    Post.find({})
+    let posts= await Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
         populate: {
             path: 'user'
         }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title:'Codeial|Home',
-                posts: posts,
-                all_Users: users
-            });
-        });
-        
     });
-}
+    
+    let users= await User.find({});
+
+    return res.render('home',{
+        title:'Codeial|Home',
+        posts: posts,
+        all_Users: users
+    });
+}catch(err){
+    console.log('Error',err);
+    return;
+}   
+} 
 
 // module.exports.actionName=function(req,res){}
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts= Post.find({}).populate('comments').exec();
+// posts.then()
