a7e15ea6161a3995f536e3c5aea244fa9d5d2618
Ashish Kaushik <ashishkaushik146@gmail.com>
2023-03-17 07:04:31 +0530

    Installing multer,configuring multer for user avatar,saving file from params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -11,13 +11,44 @@ module.exports.profile= function(req,res){
 }
 
 // action to handle upadate form
-module.exports.update= function(req,res){
+module.exports.update= async function(req,res){
+    // if(req.user.id== req.params.id){
+    //     User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+    //         req.flash('success','Profile updated successfully');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id== req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
-            req.flash('success','Profile updated successfullly');
+         
+       try{
+          let user= await User.findById(req.params.id);
+          User.uploadedAvater(req,res,function(err){
+            console.log(req.file)
+            if(err){console.log('*****Multer Error: ',err)};
+
+            user.name= req.body.name;
+            user.email= req.body.email;
+
+            if(req.file){
+                // this is saving the path of uploaded file into the avatar field in the in the user
+                user.avatar= User.avatarPath + '/' + req.file.filename;
+            }
+            user.save();
+            
             return res.redirect('back');
-        });
+          });
+       }
+
+       catch(err){
+          req.flash('error',err);
+          return res.redirect('back');
+       }
+
     }else{
+        req.flash('error','Unauthorized');
         return res.status(401).send('Unauthorized');
     }
 }
